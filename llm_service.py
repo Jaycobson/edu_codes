44dcf516746be6f8d18ce1840ba873fff9c9d633
Jaycobson <ojumubolarin@gmail.com>
2025-09-06 16:42:22 +0100

    updating

--- llm_service.py
+++ llm_service.py
@@ -2,6 +2,7 @@ import google.generativeai as genai
 import os
 from dotenv import load_dotenv
 import json
+import streamlit as st
 import re # Import the regular expression module
 
 load_dotenv()
@@ -9,7 +10,8 @@ load_dotenv()
 
 class LLMService:
     def __init__(self, model_name="gemini-2.5-flash-lite"): # Updated model name
-        api_key = os.getenv("GOOGLE_API_KEY")
+        api_key = st.secrets.get("GOOGLE_API_KEY") or os.getenv("GOOGLE_API_KEY")
+
         if not api_key:
             raise ValueError("GOOGLE_API_KEY not found in environment variables.")
         genai.configure(api_key=api_key)
